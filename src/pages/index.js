a033762a8230aea176bd6cb8874ee00fc1e54e31
Zak <87777706+zakplumridge@users.noreply.github.com>
2023-01-28 13:05:14 +0000

    CSS styling for Home Page
    Added the heading and inputs into containers so it can sit below the navigation bar. Also made adjustments to the inputs as per the wireframes.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-  
+
 const Home = () => {
   return (
-    <div>
-            <h2>Welcome to our website!</h2>
+      <div className="container">
+      <div className="heading"> <h2 class="header">Welcome to our website!</h2></div>
+      <div className="content-container">
             <p>
               Here you can search for restaurants based on your dietary
               requirements and allergens.
             </p>
-            <input placeholder="Search by Name" />
-            <input placeholder="Search by Location" />
-            <input placeholder="Search by Cuisine" />
-          </div>
+            <input class="searchbars" placeholder="Search by Name" />
+            <input class="searchbars" placeholder="Search by Location" />
+            <input class="searchbars" placeholder="Search by Allergen" />
+            </div>
+          </div> 
   );
 };
   
