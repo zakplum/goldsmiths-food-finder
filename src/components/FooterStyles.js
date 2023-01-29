a947526e15e847b34c37e96a0d3212b135533c26
Zak <87777706+zakplumridge@users.noreply.github.com>
2023-01-29 15:45:53 +0000

    Privacy Policy
    Privacy policy added - personalised to our website

--- src/components/FooterStyles.js
+++ src/components/FooterStyles.js
@@ -2,13 +2,10 @@ import styled from 'styled-components';
 import { NavLink as Link } from "react-router-dom";
    
 export const Box = styled.div`
-  padding: 20px 10px;
+  padding: 10px;
   background: white;
-  position: absolute;
+  position: sticky;
   bottom: 0;
-  width: 95%;
-  
-   
   @media (max-width: 1000px) {
     padding: 20px 10px;
   }
@@ -19,13 +16,13 @@ export const Container = styled.div`
     flex-direction: column;
     justify-content: center;
     margin: 0 auto;
-    /* background: red; */
 `
 
 export const FooterBar = styled.div`
     text-align: center;
     flex-direction: column;
     justify-content: center;
+    
 `
 export const NavLink = styled(Link)`
   color: #333;
--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -3,10 +3,196 @@ import React from "react";
 const Privacy = () => {
   return (
     <div>
-      <h1>
-        Privacy Policy
-      </h1>
+<center>
+<br></br>
+       <u><b>Privacy Notice</b></u>
+        <br></br>
+Last updated January 29, 2023
+<br></br></center>
+
+<p>
+This privacy notice for Goldsmiths Food Finder ('Company', 'we', 'us', or 'our'), describes how and why we might collect, store, use, and/or share ('process') your information when you use our services ('Services'), such as when you:
+Visit our website at goldsmithsfoodfinder.co.uk, or any website of ours that links to this privacy notice
+Engage with us in other related ways, including any sales, marketing, or events
+Questions or concerns? Reading this privacy notice will help you understand your privacy rights and choices. If you do not agree with our policies and practices, please do not use our Services. If you still have any questions or concerns, please contact us at zplum001@gold.ac.uk.
+</p>
+
+<b><u>SUMMARY OF KEY POINTS</u></b>
+<p>
+This summary provides key points from our privacy notice, but you can find out more details about any of these topics by clicking the link following each key point or by using our table of contents below to find the section you are looking for. You can also click here to go directly to our table of contents.
+</p>
+What personal information do we process? When you visit, use, or navigate our Services, we may process personal information depending on how you interact with Goldsmiths Food Finder and the Services, the choices you make, and the products and features you use. Click here to learn more.
+<br></br>
+Do we process any sensitive personal information? We do not process sensitive personal information.
+<br></br>
+Do we receive any information from third parties? We do not receive any information from third parties.
+<br></br>
+How do we process your information? We process your information to provide, improve, and administer our Services, communicate with you, for security and fraud prevention, and to comply with law. We may also process your information for other purposes with your consent. We process your information only when we have a valid legal reason to do so. Click here to learn more.
+<br></br>
+In what situations and with which parties do we share personal information? We may share information in specific situations and with specific third parties. Click here to learn more.
+<br></br>
+How do we keep your information safe? We have organisational and technical processes and procedures in place to protect your personal information. However, no electronic transmission over the internet or information storage technology can be guaranteed to be 100% secure, so we cannot promise or guarantee that hackers, cybercriminals, or other unauthorised third parties will not be able to defeat our security and improperly collect, access, steal, or modify your information. Click here to learn more.
+<br></br>
+What are your rights? Depending on where you are located geographically, the applicable privacy law may mean you have certain rights regarding your personal information. Click here to learn more.
+<br></br>
+How do you exercise your rights? The easiest way to exercise your rights is by filling out our data subject request form available here, or by contacting us. We will consider and act upon any request in accordance with applicable data protection laws.
+<br></br>
+Want to learn more about what Goldsmiths Food Finder does with any information we collect? Click here to review the notice in full.
+<br></br>
+<p>TABLE OF CONTENTS
+
+</p>
+1. WHAT INFORMATION DO WE COLLECT?<br></br>
+2. HOW DO WE PROCESS YOUR INFORMATION?<br></br>
+3. WHAT LEGAL BASES DO WE RELY ON TO PROCESS YOUR PERSONAL INFORMATION?<br></br>
+4. WHEN AND WITH WHOM DO WE SHARE YOUR PERSONAL INFORMATION?   <br></br>
+5. DO WE USE COOKIES AND OTHER TRACKING TECHNOLOGIES?<br></br>
+6. HOW DO WE HANDLE YOUR SOCIAL LOGINS?<br></br>
+7. HOW LONG DO WE KEEP YOUR INFORMATION?<br></br>
+8. HOW DO WE KEEP YOUR INFORMATION SAFE?<br></br>
+9. WHAT ARE YOUR PRIVACY RIGHTS?<br></br>
+10. CONTROLS FOR DO-NOT-TRACK FEATURES<br></br>
+11. DO CALIFORNIA RESIDENTS HAVE SPECIFIC PRIVACY RIGHTS?<br></br>
+12. DO WE MAKE UPDATES TO THIS NOTICE?<br></br>
+13. HOW CAN YOU CONTACT US ABOUT THIS NOTICE?<br></br>
+14. HOW CAN YOU REVIEW, UPDATE, OR DELETE THE DATA WE COLLECT FROM YOU?<p>
+
+1. WHAT INFORMATION DO WE COLLECT?
+<br></br>
+Personal information you disclose to us
+<br></br>
+In Short: We collect personal information that you provide to us.
+<br></br>
+We collect personal information that you voluntarily provide to us when you register on the Services, express an interest in obtaining information about us or our products and Services, when you participate in activities on the Services, or otherwise when you contact us.
+<br></br>
+Personal Information Provided by You. The personal information that we collect depends on the context of your interactions with us and the Services, the choices you make, and the products and features you use. The personal information we collect may include the following:
+names
+email addresses
+mailing addresses
+usernames
+passwords
+contact preferences
+Sensitive Information. We do not process sensitive information.
+<br></br>
+Social Media Login Data. We may provide you with the option to register with us using your existing social media account details, like your Facebook, Twitter, or other social media account. If you choose to register in this way, we will collect the information described in the section called 'HOW DO WE HANDLE YOUR SOCIAL LOGINS?' below.
+<br></br>
+All personal information that you provide to us must be true, complete, and accurate, and you must notify us of any changes to such personal information.
+<br></br>
+</p>
+<p>
+2. HOW DO WE PROCESS YOUR INFORMATION?
+<br></br>
+In Short: We process your information to provide, improve, and administer our Services, communicate with you, for security and fraud prevention, and to comply with law. We may also process your information for other purposes with your consent.
+<br></br>
+We process your personal information for a variety of reasons, depending on how you interact with our Services, including:
+To facilitate account creation and authentication and otherwise manage user accounts. We may process your information so you can create and log in to your account, as well as keep your account in working order.
+<br></br>
+To save or protect an individual's vital interest. We may process your information when necessary to save or protect an individual’s vital interest, such as to prevent harm.
+<br></br></p><p>
+3. WHAT LEGAL BASES DO WE RELY ON TO PROCESS YOUR INFORMATION?
+<br></br>
+In Short: We only process your personal information when we believe it is necessary and we have a valid legal reason (i.e. legal basis) to do so under applicable law, like with your consent, to comply with laws, to provide you with services to enter into or fulfil our contractual obligations, to protect your rights, or to fulfil our legitimate business interests.
+<br></br>
+The General Data Protection Regulation (GDPR) and UK GDPR require us to explain the valid legal bases we rely on in order to process your personal information. As such, we may rely on the following legal bases to process your personal information:
+Consent. We may process your information if you have given us permission (i.e. consent) to use your personal information for a specific purpose. You can withdraw your consent at any time. Click here to learn more.
+Legal Obligations. We may process your information where we believe it is necessary for compliance with our legal obligations, such as to cooperate with a law enforcement body or regulatory agency, exercise or defend our legal rights, or disclose your information as evidence in litigation in which we are involved.
+Vital Interests. We may process your information where we believe it is necessary to protect your vital interests or the vital interests of a third party, such as situations involving potential threats to the safety of any person.
+<br></br></p><p>
+4. WHEN AND WITH WHOM DO WE SHARE YOUR PERSONAL INFORMATION?
+<br></br>
+In Short: We may share information in specific situations described in this section and/or with the following third parties.
+<br></br>
+We may need to share your personal information in the following situations:
+Business Transfers. We may share or transfer your information in connection with, or during negotiations of, any merger, sale of company assets, financing, or acquisition of all or a portion of our business to another company.
+When we use Google Maps Platform APIs. We may share your information with certain Google Maps Platform APIs (e.g. Google Maps API, Places API). To find out more about Google’s Privacy Policy, please refer to this link. We obtain and store on your device ('cache') your location. You may revoke your consent anytime by contacting us at the contact details provided at the end of this document.</p>
+<br></br><p>
+5. DO WE USE COOKIES AND OTHER TRACKING TECHNOLOGIES?
+<br></br>
+In Short: We may use cookies and other tracking technologies to collect and store your information.
+<br></br>
+We may use cookies and similar tracking technologies (like web beacons and pixels) to access or store information. Specific information about how we use such technologies and how you can refuse certain cookies is set out in our Cookie Notice.</p>
+<br></br><p>
+6. HOW DO WE HANDLE YOUR SOCIAL LOGINS?
+<br></br>
+In Short: If you choose to register or log in to our Services using a social media account, we may have access to certain information about you.
+<br></br>
+Our Services offer you the ability to register and log in using your third-party social media account details (like your Facebook or Twitter logins). Where you choose to do this, we will receive certain profile information about you from your social media provider. The profile information we receive may vary depending on the social media provider concerned, but will often include your name, email address, friends list, and profile picture, as well as other information you choose to make public on such a social media platform.
+<br></br>
+We will use the information we receive only for the purposes that are described in this privacy notice or that are otherwise made clear to you on the relevant Services. Please note that we do not control, and are not responsible for, other uses of your personal information by your third-party social media provider. We recommend that you review their privacy notice to understand how they collect, use, and share your personal information, and how you can set your privacy preferences on their sites and apps.
+<br></br></p><p>
+7. HOW LONG DO WE KEEP YOUR INFORMATION?
+<br></br>
+In Short: We keep your information for as long as necessary to fulfil the purposes outlined in this privacy notice unless otherwise required by law.
+<br></br>
+We will only keep your personal information for as long as it is necessary for the purposes set out in this privacy notice, unless a longer retention period is required or permitted by law (such as tax, accounting, or other legal requirements). No purpose in this notice will require us keeping your personal information for longer than the period of time in which users have an account with us.
+<br></br>
+When we have no ongoing legitimate business need to process your personal information, we will either delete or anonymise such information, or, if this is not possible (for example, because your personal information has been stored in backup archives), then we will securely store your personal information and isolate it from any further processing until deletion is possible.
+<br></br></p><p>
+8. HOW DO WE KEEP YOUR INFORMATION SAFE?
+<br></br>
+In Short: We aim to protect your personal information through a system of organisational and technical security measures.
+<br></br>
+We have implemented appropriate and reasonable technical and organisational security measures designed to protect the security of any personal information we process. However, despite our safeguards and efforts to secure your information, no electronic transmission over the Internet or information storage technology can be guaranteed to be 100% secure, so we cannot promise or guarantee that hackers, cybercriminals, or other unauthorised third parties will not be able to defeat our security and improperly collect, access, steal, or modify your information. Although we will do our best to protect your personal information, transmission of personal information to and from our Services is at your own risk. You should only access the Services within a secure environment.
+<br></br></p><p>
+9. WHAT ARE YOUR PRIVACY RIGHTS?
+<br></br>
+In Short: In some regions, such as the European Economic Area (EEA) and United Kingdom (UK), you have rights that allow you greater access to and control over your personal information. You may review, change, or terminate your account at any time.
+<br></br>
+In some regions (like the EEA and UK), you have certain rights under applicable data protection laws. These may include the right (i) to request access and obtain a copy of your personal information, (ii) to request rectification or erasure; (iii) to restrict the processing of your personal information; and (iv) if applicable, to data portability. In certain circumstances, you may also have the right to object to the processing of your personal information. You can make such a request by contacting us by using the contact details provided in the section 'HOW CAN YOU CONTACT US ABOUT THIS NOTICE?' below.
+<br></br>
+We will consider and act upon any request in accordance with applicable data protection laws.
+<br></br>
+If you are located in the EEA or UK and you believe we are unlawfully processing your personal information, you also have the right to complain to your local data protection supervisory authority. You can find their contact details here: https://ec.europa.eu/justice/data-protection/bodies/authorities/index_en.htm.
+<br></br>
+If you are located in Switzerland, the contact details for the data protection authorities are available here: https://www.edoeb.admin.ch/edoeb/en/home.html.
+<br></br>
+Withdrawing your consent: If we are relying on your consent to process your personal information, you have the right to withdraw your consent at any time. You can withdraw your consent at any time by contacting us by using the contact details provided in the section 'HOW CAN YOU CONTACT US ABOUT THIS NOTICE?' below or updating your preferences.
+<br></br>
+However, please note that this will not affect the lawfulness of the processing before its withdrawal nor, will it affect the processing of your personal information conducted in reliance on lawful processing grounds other than consent.
+<br></br>
+Account Information
+<br></br>
+If you would at any time like to review or change the information in your account or terminate your account, you can:
+Contact us using the contact information provided.
+Upon your request to terminate your account, we will deactivate or delete your account and information from our active databases. However, we may retain some information in our files to prevent fraud, troubleshoot problems, assist with any investigations, enforce our legal terms and/or comply with applicable legal requirements.
+<br></br>
+Cookies and similar technologies: Most Web browsers are set to accept cookies by default. If you prefer, you can usually choose to set your browser to remove cookies and to reject cookies. If you choose to remove cookies or reject cookies, this could affect certain features or services of our Services. To opt out of interest-based advertising by advertisers on our Services visit http://www.aboutads.info/choices/.
+<br></br>
+If you have questions or comments about your privacy rights, you may email us at zplum001@gold.ac.uk.
+<br></br></p><p>
+10. CONTROLS FOR DO-NOT-TRACK FEATURES
+<br></br>
+Most web browsers and some mobile operating systems and mobile applications include a Do-Not-Track ('DNT') feature or setting you can activate to signal your privacy preference not to have data about your online browsing activities monitored and collected. At this stage no uniform technology standard for recognising and implementing DNT signals has been finalised. As such, we do not currently respond to DNT browser signals or any other mechanism that automatically communicates your choice not to be tracked online. If a standard for online tracking is adopted that we must follow in the future, we will inform you about that practice in a revised version of this privacy notice.
+<br></br></p><p>
+11. DO CALIFORNIA RESIDENTS HAVE SPECIFIC PRIVACY RIGHTS?
+<br></br>
+In Short: Yes, if you are a resident of California, you are granted specific rights regarding access to your personal information.
+<br></br>
+California Civil Code Section 1798.83, also known as the 'Shine The Light' law, permits our users who are California residents to request and obtain from us, once a year and free of charge, information about categories of personal information (if any) we disclosed to third parties for direct marketing purposes and the names and addresses of all third parties with which we shared personal information in the immediately preceding calendar year. If you are a California resident and would like to make such a request, please submit your request in writing to us using the contact information provided below.
+<br></br>
+If you are under 18 years of age, reside in California, and have a registered account with Services, you have the right to request removal of unwanted data that you publicly post on the Services. To request removal of such data, please contact us using the contact information provided below and include the email address associated with your account and a statement that you reside in California. We will make sure the data is not publicly displayed on the Services, but please be aware that the data may not be completely or comprehensively removed from all our systems (e.g. backups, etc.).
+<br></br></p><p>
+12. DO WE MAKE UPDATES TO THIS NOTICE?
+<br></br>
+In Short: Yes, we will update this notice as necessary to stay compliant with relevant laws.
+<br></br>
+We may update this privacy notice from time to time. The updated version will be indicated by an updated 'Revised' date and the updated version will be effective as soon as it is accessible. If we make material changes to this privacy notice, we may notify you either by prominently posting a notice of such changes or by directly sending you a notification. We encourage you to review this privacy notice frequently to be informed of how we are protecting your information.
+<br></br></p><p>
+13. HOW CAN YOU CONTACT US ABOUT THIS NOTICE?
+<br></br>
+If you have questions or comments about this notice, you may email us at zplum001@gold.ac.uk or by post to:
+<br></br>
+Goldsmiths Food Finder
+8 Lewisham Way
+London SE14 6NW 
+England
+<br></br></p><p>
+14. HOW CAN YOU REVIEW, UPDATE, OR DELETE THE DATA WE COLLECT FROM YOU?
+<br></br>
+Based on the applicable laws of your country, you may have the right to request access to the personal information we collect from you, change that information, or delete it. To request to review, update, or delete your personal information, please submit a request form by clicking here.
+This privacy policy was created using Termly's Privacy Policy Generator.</p>
     </div>
+    
   );
 };
   
